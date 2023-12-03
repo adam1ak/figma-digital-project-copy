66cd341482dc24864191f3fb7003928326ada2a8
adam1ak <f.admiakk@gmail.com>
2023-12-03 16:40:03 +0100

    Add popup with link to design on Figma

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-// Select the header element
-var header = document.querySelector('header');
-
-// Add scroll event listener to window
-window.addEventListener('scroll', function() {
-    // Check if scrolled down
-    if (window.scrollY > 50) {
-        // Add 'scrolled' class to header
-        header.classList.add('scrolled');
-    } else {
-        // Remove 'scrolled' class from header
-        header.classList.remove('scrolled');
-    }
-});
 var navbar = document.querySelector('.mobile-navbar');
 var bgHide = document.querySelector('.background-hide');
 var mainElement = document.querySelector('main');
 var burger = document.querySelector('.burger');
 var hedear = document.querySelector('header');
 var body = document.querySelector('body');
+var header = document.querySelector('header');
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+            header.style.filter = "blur(5px)";
+            mainElement.classList.toggle('blur')
+            body.style.overflow = 'hidden';
+        },
+        750
+    )
+});
+
+
+document.querySelector("#popup-close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+    header.style.filter = "none";
+    mainElement.classList.remove('blur')
+    body.style.overflow = 'visible';
+});
+document.querySelector("#popup-link-close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+    header.style.filter = "none";
+    mainElement.classList.remove('blur')
+    body.style.overflow = 'visible';
+});
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
 document.querySelector('.burger-click').addEventListener('click', function() {
     navbar.classList.toggle('show');
     mainElement.classList.toggle('blur');
